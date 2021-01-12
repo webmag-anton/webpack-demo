0d0d34a0a9bcf75c59ba8f5e97f3468c450177dc
Anton <anton.byshok@gmail.com>
2021-01-12 13:37:15 +0200

    1

--- conspekt.js
+++ conspekt.js
@@ -92,7 +92,7 @@ node-sass                                    корневой функциона
 sass-loader                                  для взаимодействия webpack и sass
 @babel/core                                  корневой функционал babel
 babel-loader                                 для взаимодействия webpack и babel
-@babel/preset-env                            preset-env включает преобразования для ES2015+ (все последние фишки)
+@babel/preset-env                            preset-env включает все плагины для преобразования ES2015+ в ES5
 @babel/polyfill                              для использования async/await (полифилы устанавливаются в dependencies)
-@babel/plugin-proposal-class-properties      для понимания babel статических свойства/методы классов
+@babel/plugin-proposal-class-properties      плагин для транспиляции статических свойств/методов класса
 webpack-bundle-analyzer                      анализирует размер файлов и библиотек в приложении и воводит инфографику
